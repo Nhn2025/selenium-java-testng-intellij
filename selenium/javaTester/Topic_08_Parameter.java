767a03f5bc93aa4b8a397d7131143765a1e05087
Nhn2025 <dnhu122025@gmail.com>
2024-08-09 14:17:10 +0700

    add Topic_08_Parameter

---
+++ selenium/javaTester/Topic_08_Parameter.java
@@ -0,0 +1,24 @@
+package javaTester;
+
+public class Topic_08_Parameter {
+
+    static String fullNameGlobal = "Automation Testing";
+
+    public static void main(String[] args) {
+
+        System.out.println(getFullName());
+
+        // Đối số
+        setFullName("Manual Testing");
+
+        System.out.println(getFullName());
+    }
+
+    public static void setFullName(String fullName) { // Tham số
+        fullNameGlobal = fullName;
+    }
+
+    public static String getFullName() {
+        return fullNameGlobal;
+    }
+}
