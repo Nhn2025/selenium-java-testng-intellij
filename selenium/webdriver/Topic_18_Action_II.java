eed519a3c882311422394339c3bdad4dcd921258
Nhn2025 <dnhu122025@gmail.com>
2024-08-17 10:03:11 +0700

    add Topic_18_Action_II

---
+++ selenium/webdriver/Topic_18_Action_II.java
@@ -0,0 +1,98 @@
+package webdriver;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Topic_18_Action_II {
+
+    WebDriver driver;
+    Actions action;
+    String osName = System.getProperty("os.name");
+    Keys keys;
+
+    @BeforeClass
+    public void initialBrowser() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+        action = new Actions(driver);
+        keys = osName.startsWith("Windows") ?  Keys.CONTROL : Keys.COMMAND;
+    }
+
+    @Test
+    public void TC_01_Click_And_Hold_Block () throws InterruptedException {
+        driver.get("https://automationfc.github.io/jquery-selectable/");
+
+        List<WebElement> allNumber = driver.findElements(By.cssSelector("ol#selectable>li"));
+        Assert.assertEquals(allNumber.size(), 20);
+
+        // Thao tác với nhiều element
+        // System.out.println(allNumber.get(0).getText());
+
+        action.clickAndHold(allNumber.get(0)) // Click vào số 1 và giữ chuột
+                .moveToElement(allNumber.get(3)) // Di chuột tới số 4
+                .release() // Nhả chuột trái ra - kết thúc cho sự kiện ClickAndHold()
+                .perform(); // Thực thi các câu lệnh trên (nếu ko có thì ko thực thi)
+        Thread.sleep(4000);
+
+        List<WebElement> allNumberSelected = driver.findElements(By.cssSelector("ol#selectable>li.ui-selected"));
+        Assert.assertEquals(allNumberSelected.size(), 4);
+    }
+
+    @Test
+    public void TC_02_Click_And_Hold_Romdom() {
+        driver.get("https://automationfc.github.io/jquery-selectable/");
+
+        List<WebElement> allNumber = driver.findElements(By.cssSelector("ol#selectable>li"));
+        Assert.assertEquals(allNumber.size(), 20);
+
+        // Nhấn phím Ctrl xuống (chưa nhả ra)
+        action.keyDown(keys).perform();
+
+        action.click(allNumber.get(0))
+                .click(allNumber.get(3))
+                .click(allNumber.get(7))
+                .click(allNumber.get(10))
+                .click(allNumber.get(13))
+                .click(allNumber.get(17))
+                .pause(Duration.ofSeconds(3))
+                .perform();
+        // 1 4 8 11 14 18
+
+        // Nhả phím Ctrl ra
+        action.keyUp(Keys.CONTROL).perform();
+
+        List<WebElement> allNumberSelected = driver.findElements(By.cssSelector("ol#selectable>li.ui-selected"));
+        Assert.assertEquals(allNumberSelected.size(), 6);
+    }
+
+    @Test
+    public void TC_03_Double_Click() {
+        driver.get("https://automationfc.github.io/basic-form/index.html");
+
+        action.doubleClick(driver.findElement(By.xpath("//button[text()='Double click me']"))).perform();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("p#demo")).getText(), "Hello Automation Guys!");
+    }
+
+    @AfterClass
+    public void cleanBrowser() {
+        driver.quit();
+    }
+
+    public void sleepInSeconds(long timeInSecond) {
+        try {
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
