5c2c2592f2ba6d9f33c39f0aa71ed64698543f0c
Nhn2025 <dnhu122025@gmail.com>
2024-08-17 14:21:33 +0700

    add Topic_16_Action_III

---
+++ selenium/webdriver/Topic_16_Action_III.java
@@ -0,0 +1,185 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.Color;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.time.Duration;
+
+public class Topic_16_Action_III {
+
+    WebDriver driver;
+    Actions action;
+    JavascriptExecutor jsExecutor;
+    String projectPath = System.getProperty("user.dir");
+
+    @BeforeClass
+    public void initialBrowser() {
+        // Firefox hay bị vấn đề về Viewport
+        // Nếu element không nằm trong Viewport sẽ không scroll được
+        // driver = new FirefoxDriver();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+
+        action = new Actions(driver);
+        // Ép kiểu
+        jsExecutor = (JavascriptExecutor) driver;
+    }
+
+    @Test
+    public void TC_01_Drag_Drop_HTML4() {
+        driver.get("https://automationfc.github.io/kendo-drag-drop/");
+
+        WebElement sourceCircle = driver.findElement(By.cssSelector("div#draggable"));
+        WebElement targetCircle = driver.findElement(By.cssSelector("div#droptarget"));
+
+        action.dragAndDrop(sourceCircle, targetCircle).perform();
+        sleepInSeconds(3);
+
+        Assert.assertEquals(targetCircle.getText(), "You did great!");
+        Assert.assertEquals(Color.fromString(targetCircle.getCssValue("background-color")).asHex().toUpperCase(), "#03A9F4");
+    }
+
+    @Test
+    public void TC_02_Drag_Drop_HTML5() throws IOException {
+        driver.get("https://automationfc.github.io/drag-drop-html5/");
+
+        // Site ko support JQuery
+        // String jQueryLibraries = getContentFile(projectPath + "\\drapDrop\\jQueryLib.js");
+        // jsExecutor.executeScript(jQueryLibraries);
+
+        // Sử dụng cách này để tránh code bị dài
+        String jqueryDrapDropContent = getContentFile(projectPath + "\\drapDrop\\drapAndDrop.js");
+
+        // Drap A to B
+        jsExecutor.executeScript(jqueryDrapDropContent);
+        sleepInSeconds(3);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#column-a>header")).getText(), "B");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#column-b>header")).getText(), "A");
+
+        // Drap B to A
+        jsExecutor.executeScript(jqueryDrapDropContent);
+        sleepInSeconds(3);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#column-a>header")).getText(), "A");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#column-b>header")).getText(), "B");
+    }
+
+    @Test
+    public void TC_03_Drag_Drop_HTML5_Java_Robot() throws AWTException {
+        // Chạy không ổn định (do scale cao)
+        driver.get("https://automationfc.github.io/drag-drop-html5/");
+
+        dragAndDropHTML5ByXpath("div#column-a", "div#column-b");
+        sleepInSeconds(3);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#column-a>header")).getText(), "B");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#column-b>header")).getText(), "A");
+
+        dragAndDropHTML5ByXpath("div#column-a", "div#column-b");
+        sleepInSeconds(3);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#column-a>header")).getText(), "A");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#column-b>header")).getText(), "B");
+    }
+
+    @Test
+    public void TC_04_Scroll_To_ELement() {
+        driver.get("http://live.techpanda.org/index.php/about-magento-demo-store/");
+
+        // Firefox hay bị vấn đề về Viewport
+        // Nếu element không nằm trong Viewport sẽ không scroll được
+        // Chrome và Edge vẫn bình thường
+        action.scrollToElement(driver.findElement(By.cssSelector("input#newsletter"))).perform();
+        sleepInSeconds(3);
+    }
+
+    // Hàm đọc file để thực thi JQuery tránh bị dài
+    public String getContentFile(String filePath) throws IOException {
+        Charset cs = Charset.forName("UTF-8");
+        FileInputStream stream = new FileInputStream(filePath);
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(stream, cs));
+            StringBuilder builder = new StringBuilder();
+            char[] buffer = new char[8192];
+            int read;
+            while ((read = reader.read(buffer, 0, buffer.length)) > 0) {
+                builder.append(buffer, 0, read);
+            }
+            return builder.toString();
+        } finally {
+            stream.close();
+        }
+    }
+
+    public void dragAndDropHTML5ByXpath(String sourceLocator, String targetLocator) throws AWTException {
+
+        WebElement source = driver.findElement(By.cssSelector(sourceLocator));
+        WebElement target = driver.findElement(By.cssSelector(targetLocator));
+
+        // Setup robot
+        Robot robot = new Robot();
+        robot.setAutoDelay(500);
+
+        // Get size of elements
+        org.openqa.selenium.Dimension sourceSize = source.getSize();
+        org.openqa.selenium.Dimension targetSize = target.getSize();
+
+        // Get center distance
+        int xCentreSource = sourceSize.width / 2;
+        int yCentreSource = sourceSize.height / 2;
+        int xCentreTarget = targetSize.width / 2;
+        int yCentreTarget = targetSize.height / 2;
+
+        org.openqa.selenium.Point sourceLocation = source.getLocation();
+        org.openqa.selenium.Point targetLocation = target.getLocation();
+
+        // Make Mouse coordinate center of element
+        sourceLocation.x += 20 + xCentreSource;
+        sourceLocation.y += 110 + yCentreSource;
+        targetLocation.x += 20 + xCentreTarget;
+        targetLocation.y += 110 + yCentreTarget;
+
+        // Move mouse to drag from location
+        robot.mouseMove(sourceLocation.x, sourceLocation.y);
+
+        // Click and drag
+        robot.mousePress(InputEvent.BUTTON1_MASK);
+        robot.mousePress(InputEvent.BUTTON1_MASK);
+        robot.mouseMove(((sourceLocation.x - targetLocation.x) / 2) + targetLocation.x, ((sourceLocation.y - targetLocation.y) / 2) + targetLocation.y);
+
+        // Move to final position
+        robot.mouseMove(targetLocation.x, targetLocation.y);
+
+        // Drop
+        robot.mouseRelease(InputEvent.BUTTON1_MASK);
+    }
+
+    @AfterClass
+    public void cleanBrowser() {
+        driver.quit();
+    }
+
+    public void sleepInSeconds(long timeInSecond) {
+        try {
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
