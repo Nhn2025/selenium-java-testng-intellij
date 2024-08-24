b4d6bb375ec1ce5004ba6a23b6dcaa47d22f38ee
Nhn2025 <dnhu122025@gmail.com>
2024-08-24 16:00:10 +0700

    update upload file

--- selenium/javaTester/Topic_03_System_Info.java
+++ selenium/javaTester/Topic_03_System_Info.java
@@ -1,12 +1,20 @@
 package javaTester;
 
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.Platform;
+
+import java.io.File;
 
 public class Topic_03_System_Info {
     public static void main(String args[]) {
 
         // Lấy ra đường dẫn tương đối tại thư mục hiện tại
         String projectPath = System.getProperty("user.dir");
+
+        String fileName = "tho.jpg";
+
+        System.out.println(projectPath + "\\uploadFiles\\" + fileName);
+
         System.out.println(projectPath + "\\browserDrivers");
 
         String osName = System.getProperty("os.name");
@@ -16,5 +24,22 @@ public class Topic_03_System_Info {
             keys = Keys.CONTROL;
         else
             keys = Keys.COMMAND;
+
+        Keys cmdCtrl = Platform.getCurrent().is(Platform.WINDOWS) ? Keys.CONTROL : Keys.COMMAND;
+
+       // String character = Platform.getCurrent().is(Platform.WINDOWS) ? "\\" : "/";
+        String character = File.separator;
+
+        String hcmName = "tho.jpg";
+        String hnName = "avatar.jpg";
+        String dnName = "sua.jpg";
+
+        String hcmFilePath = projectPath + character + "uploadFiles" + character + hcmName;
+        String hnFilePath = projectPath + character + "uploadFiles" + character + hnName;
+        String dnFilePath = projectPath + character + "uploadFiles" + character + dnName;
+
+        System.out.println(hcmFilePath);
+        System.out.println(hnFilePath);
+        System.out.println(dnFilePath);
     }
 }
