2777d7d194bcc170be11a52f630b6213bdd34ff1
Nhn2025 <dnhu122025@gmail.com>
2024-08-31 21:31:49 +0700

    add Topic_15_Generic

---
+++ selenium/javaTester/Topic_15_Generic.java
@@ -0,0 +1,24 @@
+package javaTester;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Topic_15_Generic {
+
+    public static void main(String[] args) {
+        // Generic: List chỉ chứa kiểu dữ liệu String
+        // E T V X I: the type of elements in this list
+        List<String> students = new ArrayList<String>();
+        students.add("John");
+        students.add("Mary");
+        students.add("Peter");
+
+        // non-Generic
+        ArrayList address = new ArrayList<>();
+        address.add("123 Main st."); // String
+        address.add(15); // Integer
+        address.add(true); // Booleans
+        address.add(15.56); // Float
+    }
+
+}
