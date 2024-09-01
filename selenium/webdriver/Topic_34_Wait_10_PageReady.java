c78651f39231b9aac93ab99cbdadd39e94adaad2
Nhn2025 <dnhu122025@gmail.com>
2024-09-01 22:17:51 +0700

    add Topic_34_Wait_10_PageReady

---
+++ selenium/webdriver/Topic_34_Wait_10_PageReady.java
@@ -0,0 +1,100 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_34_Wait_10_PageReady {
+
+    WebDriver driver;
+
+    WebDriverWait explicitWait;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    // Fail do trang mới thêm captcha
+    @Test
+    public void TC_01_Admin_NopCommerce() {
+        driver.get("https://admin-demo.nopcommerce.com/");
+
+        driver.findElement(By.cssSelector("input#Email")).clear();
+        driver.findElement(By.cssSelector("input#Email")).sendKeys("admin@yourstore.com");
+
+        driver.findElement(By.cssSelector("input#Password")).clear();
+        driver.findElement(By.cssSelector("input#Password")).sendKeys("admin");
+        driver.findElement(By.cssSelector("button.login-button")).click();
+        // Sleep cho hiện loading icon
+        sleepInSeconds(2);
+
+        Assert.assertTrue(isPageLoadedSuccess());
+
+        explicitWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//i[contains(@class,'fa-user')]/following-sibling::p"))).click();
+        driver.findElement(By.xpath("//li[contains(@class,'menu-is-opening')]/a/p[contains(string(),'Customers')]")).click();
+        Assert.assertTrue(isPageLoadedSuccess());
+
+        driver.findElement(By.xpath("//i[contains(@class,'fa-book')]/following-sibling::p")).click();
+        driver.findElement(By.xpath("//li[contains(@class,'menu-is-opening')]//p[contains(string(),'Products')]")).click();
+        Assert.assertTrue(isPageLoadedSuccess());
+
+        driver.findElement(By.xpath("//i[contains(@class,'fa-shopping-cart')]/following-sibling::p")).click();
+        driver.findElement(By.xpath("//li[contains(@class,'menu-is-opening')]//p[contains(string(),'Orders')]")).click();
+        Assert.assertTrue(isPageLoadedSuccess());
+
+        driver.findElement(By.xpath("//a[text()='Logout']")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//strong[text()='Welcome, please sign in!']")).isDisplayed());
+    }
+
+    // Tại sao dùng implicit cũng pass
+    // Do nó có sẵn trong DOM
+    // Chỉ cần wait tìm element
+    @Test
+    public void TC_02_OrangeHRM_API_Document() {
+        driver.get("https://api.orangehrm.com/");
+
+        Assert.assertTrue(explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//h1[text()='OrangeHRM REST API Documentation']"))));
+
+        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        // Assert.assertTrue(driver.findElement(By.xpath("//h1[text()='OrangeHRM REST API Documentation']")).isDisplayed());
+    }
+
+    public boolean waitAjaxLoadingInvisible() {
+        return explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div#ajaxBusy")));
+    }
+
+    public boolean isPageLoadedSuccess() {
+        return new WebDriverWait(driver, Duration.ofSeconds(30)).until(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver driver) {
+                return (Boolean) ((JavascriptExecutor) driver)
+                        .executeScript("return (window.jQuery != null) && (jQuery.active === 0);");
+            }
+        });
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+    public void sleepInSeconds(long timeInSecond) {
+        try {
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
