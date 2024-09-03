ee4826e680f9ff2fdcb6a9f5f9d95e9cc675dcdb
Nhn2025 <dnhu122025@gmail.com>
2024-09-03 22:28:26 +0700

    update comment

--- selenium/testng/Topic_01_Sample.java
+++ selenium/testng/Topic_01_Sample.java
@@ -1,22 +1,40 @@
 package testng;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.Random;
 
 public class Topic_01_Sample {
 
-    //Unit Test
-    @Test
-    public void testGetRamdomNumber() {
-        Topic_01_Sample sample = new Topic_01_Sample();
-        int randomNumber = sample.getRamdomNumber();
-        Assert.assertTrue(randomNumber >= 0 && randomNumber < 1000000);
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        //driver.manage().window().maximize();
     }
 
-    // Component
-    private int getRamdomNumber() {
-        return new Random().nextInt(1000000);
+    @Test
+    public void TC_01_() {
+
     }
+
+    @Test
+    public void TC_02_() {
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
 }
