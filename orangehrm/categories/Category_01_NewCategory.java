1334b78d9fd1f22e337feb6f678949446af2d448
Nhn2025 <dnhu122025@gmail.com>
2024-09-03 22:28:02 +0700

    add Category_Customer_Order

---
+++ orangehrm/categories/Category_01_NewCategory.java
@@ -0,0 +1,21 @@
+package categories;
+
+import org.testng.annotations.Test;
+
+public class Category_01_NewCategory {
+
+    @Test(groups = "category")
+    public void testCreateNewCategoryEmpty() {
+
+    }
+
+    @Test(groups = "category")
+    public void testCreateNewCategoryNameAndDescription() {
+
+    }
+
+    @Test(groups = "category")
+    public void testCreateNewCategoryWithParentCategory() {
+
+    }
+}
