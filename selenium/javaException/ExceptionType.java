ce5db1cc072186f4b7888a3fc60846ba3050383d
Nhn2025 <dnhu122025@gmail.com>
2024-09-27 16:23:25 +0700

    rename

--- selenium/javaException/ExceptionType.java
+++ selenium/javaException/ExceptionType.java
@@ -1,16 +1,14 @@
 package javaException;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 import java.io.*;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
-public class HandleException {
+public class ExceptionType {
 
     FirefoxDriver driver;
 
